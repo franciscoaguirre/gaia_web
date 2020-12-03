2b760e0bb89c1b86373eb843232bc26692b2172a
Francisco Aguirre <francisco.aguirre@fing.edu.uy>
2020-12-03 15:08:25 -0300

    Fix typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-Carrierwave.configure do |config|
+CarrierWave.configure do |config|
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
   else
     config.storage = :fog
     config.fog_credentials = {
-      provide: 'AWS',
+      provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION']
