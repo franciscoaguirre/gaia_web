d662b2ab29c9fbf918e413878b8d1aeea9fa68ea
Francisco Aguirre <francisco.aguirre@fing.edu.uy>
2020-12-04 11:37:56 -0300

    Add popovers to welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @carpincho = Product.find_by(name: 'Carpincho')
+    @coati = Product.find_by(name: 'Coati')
+    @tamandua = Product.find_by(name: 'Tamandua')
+  end
 end
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,7 +1,31 @@
+import 'bootstrap/js/dist/popover';
+
 $(document).ready(() => {
-  $('.animal').tooltip({
-    animated: 'fade',
-    placement: 'top-end',
-    trigger: 'manual'
+  const popoverShown = [false, null];
+
+  $('[data-toggle="popover"]').popover({
+    template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body row no-gutters justify-content-end"></div></div>',
+    placement: 'top',
+    html: true,
+  });
+
+  $('[data-toggle="popover"]').on('shown.bs.popover', (event) => {
+    popoverShown[0] = true;
+    popoverShown[1] = $(event.target);
+  });
+
+  $(document).on('click', (event) => {
+    if (!popoverShown) {
+      return;
+    }
+
+    const $target = $(event.target);
+    if ($target.parents('.popover').length === 0) {
+      if (popoverShown[1]) {
+        popoverShown[1].popover('hide');
+      }
+
+      popoverShown[0] = false;
+    }
   });
 });
